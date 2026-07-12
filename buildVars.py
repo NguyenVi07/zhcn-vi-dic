29901ca29757f3335b5be9ee26ed7f2d6d2400ef
KtGame <vnuyen57@gmail.com>
2026-07-12 08:20:25 +0700

    Release v1.0.5 with NVDA 2026 support
    Bump version to 1.0.5 and update compatibility with NVDA 2026. Update the minimum tested NVDA version from 2025.3.3 to 2026.1. Update changelog accordingly. Also remove VSCode workspace configuration files.

--- buildVars.py
+++ buildVars.py
@@ -24,11 +24,11 @@ addon_info = AddonInfo(
 	addon_description=_("""Add-on từ điển Trung-Việt chuyên biệt dành cho game thủ khiếm thị trải nghiệm các tựa game Trung Quốc qua NVDA.
 Dữ liệu từ điển được phát triển bởi cộng đồng Convert truyện Trung-Việt và được chuyển sang định dạng tương thích NVDA bởi Lê Trọng Tấn"""),
 	# version
-	addon_version="1.0.4",
+	addon_version="1.0.5",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_("""1.0.2
-dùng AddonTemplate"""),
+	addon_changelog=_("""1.0.5
+tương thích với nvda 2026""),
 	# Author(s)
 	addon_author="KtGame",
 	# URL for the add-on documentation support
@@ -40,7 +40,7 @@ dùng AddonTemplate"""),
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
 	addon_minimumNVDAVersion="2024.1",
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	addon_lastTestedNVDAVersion="2025.3.3",
+	addon_lastTestedNVDAVersion="2026.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
--- changelog.md
+++ changelog.md
@@ -1,9 +1,2 @@
-## v1.0.4
-
-* Cập nhật từ điển tiếng Việt cho các mục từ thông dụng để trở nên tự nhiên hơn:
-
-  * 列表 (liệt biểu) -> danh sách
-  * 按钮 (cái nút) -> nút
-  * 登录 (đăng lục) -> đăng nhập
-  * 设置 (thiết trí) -> cài đặt
-  * 
+## v1.0.5
+tương thích với nvda 2026
